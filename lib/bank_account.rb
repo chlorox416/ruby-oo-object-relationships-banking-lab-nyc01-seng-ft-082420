4ca1a5461ab5516a6c540094fcad362549dc569d
chlorox416 <chloechang416@gmail.com>
2020-08-28 14:42:36 -0400

    almost

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,37 @@
+# require 'pry'
 class BankAccount
+
+    attr_reader :name
+    attr_accessor :balance, :status 
+
+    @@all = []
+
+    def initialize(name)
+        @name = name 
+        @balance = 1000 
+        @status = "open"
+        @@all<< self 
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def deposit(dep_amount)
+        self.balance += dep_amount
+    end 
+    
+    def display_balance
+        "Your balance is $#{@balance}."
+    end
+
+    def valid?
+        balance > 0 && status == "open"
+    end 
+    
+    def close_account
+        self.status = "closed"
+    end
+# binding.pry
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,35 @@
+
 class Transfer
-  # your code here
+
+  attr_accessor :status 
+  attr_reader :amount, :sender, :receiver
+  @@all =[]
+
+  def initialize(sender,receiver,amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+    @@all << self 
+
+  end 
+
+  def self.all
+    @@all 
+  end 
+  
+  def valid?
+    receiver.valid? && sender.valid? 
+  end 
+
+  def execute_transaction
+  end 
+
+  def reverse_transfer 
+  end 
+  
+  
+
+
+
 end
